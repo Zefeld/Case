9476254ec81cd2d8b23d50d6ebbb013f2690def4
W009y <kiruhabarinovvk@gmail.com>
2024-11-19 02:21:36 +0300

    vk_video

--- src/tasks.js
+++ src/tasks.js
@@ -2,10 +2,7 @@
 var modal = document.querySelector("#open_everyDay");
 var btn = document.querySelector(".every_day_tasks");
 var close = document.querySelector("#krest");
-
-console.log(modal); // Должен быть элемент, а не null
-console.log(btn); // Должен быть элемент, а не null
-console.log(close);
+var close2 = document.querySelector("#claim");
 
 // Открыть модальное окно при нажатии на кнопку
 btn.onclick = function() {
@@ -16,6 +13,9 @@ close.onclick = function() {
     modal.style.display = "none"; // Закрываем модальное окно
 }
 
+close2.onclick = function() {
+    modal.style.display = "none"; // Закрываем модальное окно
+}
 
 // Закрыть модальное окно при нажатии на элемент <span>
 
@@ -23,5 +23,24 @@ close.onclick = function() {
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none"; // Закрываем модальное окно
+    }
+}
+
+
+var open_t1 = document.querySelector("#open_taskOne");
+var task1 = document.querySelector("#task_one");
+var cl1 = document.querySelector(".claim_task");
+
+task1.onclick = function() {
+    open_t1.style.display = "block";
+}
+
+cl1.onclick = function() {
+    open_t1.style.display = "none"; // Закрываем модальное окно
+}
+
+window.onclick = function(event) {
+    if (event.target == open_t1) {
+        open_t1.style.display = "none"; // Закрываем модальное окно
     }
 }
