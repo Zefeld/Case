f1651db755da53b5c73b11a2b0b0d46fa63c44eb
W009y <kiruhabarinovvk@gmail.com>
2024-11-19 23:46:25 +0300

    tasks

--- src/tasks.js
+++ src/tasks.js
@@ -44,3 +44,77 @@ window.onclick = function(event) {
         open_t1.style.display = "none"; // Закрываем модальное окно
     }
 }
+
+
+var open_t2 = document.querySelector("#open_taskTwo");
+var task2 = document.querySelector("#task_two");
+var cl2 = document.querySelector(".claim_task2");
+
+task2.onclick = function() {
+    open_t2.style.display = "block";
+}
+
+cl2.onclick = function() {
+    open_t2.style.display = "none"; // Закрываем модальное окно
+}
+
+window.onclick = function(event) {
+    if (event.target == open_t2) {
+        open_t2.style.display = "none"; // Закрываем модальное окно
+    }
+}
+
+
+var open_t3 = document.querySelector("#open_taskThree");
+var task3 = document.querySelector("#task_three");
+var cl3 = document.querySelector(".claim_task3");
+
+task3.onclick = function() {
+    open_t3.style.display = "block";
+}
+
+cl3.onclick = function() {
+    open_t3.style.display = "none"; // Закрываем модальное окно
+}
+
+window.onclick = function(event) {
+    if (event.target == open_t3) {
+        open_t3.style.display = "none"; // Закрываем модальное окно
+    }
+}
+
+var open_t4 = document.querySelector("#open_taskFour");
+var task4 = document.querySelector("#task_four");
+var cl4 = document.querySelector(".claim_task4");
+
+task4.onclick = function() {
+    open_t4.style.display = "block";
+}
+
+cl4.onclick = function() {
+    open_t4.style.display = "none"; // Закрываем модальное окно
+}
+
+window.onclick = function(event) {
+    if (event.target == open_t4) {
+        open_t4.style.display = "none"; // Закрываем модальное окно
+    }
+}
+
+var open_t5 = document.querySelector("#open_taskFive");
+var task5 = document.querySelector("#task_five");
+var cl5 = document.querySelector(".claim_task5");
+
+task5.onclick = function() {
+    open_t5.style.display = "block";
+}
+
+cl5.onclick = function() {
+    open_t5.style.display = "none"; // Закрываем модальное окно
+}
+
+window.onclick = function(event) {
+    if (event.target == open_t5) {
+        open_t5.style.display = "none"; // Закрываем модальное окно
+    }
+}
